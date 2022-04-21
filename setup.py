7ff96a82d6825b2f3e2b7210e90e572d124420bc
Andy Dang <andy@whylabs.ai>
2022-04-21 19:52:25 +0000

    Bump to dev8

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-
-# Modified from:
-# http://www.benjack.io/2018/02/02/python-cpp-revisited.html
-
 import os
 import sys
 import sysconfig
@@ -81,10 +61,10 @@ class CMakeBuild(build_ext):
 
 setup(
     name='whylogs-sketching',
-    version='3.4.0.dev7',
+    version='3.4.0.dev8',
     author='whylogs team',
     author_email='support@whylabs.ai',
-    description='sketching library of whylogs. Fork of Apache Datasketches',
+    description='sketching library of whylogs',
     license='Apache License 2.0',
     url='https://github.com/whylabs/whylogs-sketching',
     long_description=open('python/README.md').read(),
